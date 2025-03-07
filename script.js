718bd4f40ccf76cbaed1091f42db7edb44f86b56
Fernando Fantinel <fantinel89@gmail.com>
2025-03-07 20:40:04 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const image = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    image.setAttribute("src", "./assets/avatar-light.png")
+    image.setAttribute(
+      "alt",
+      "Foto de Fernando Fantinel sorrindo usando óculos de sol, alargadores, camiseta preta e barba em fundo roxo e azul com degradê."
+    )
+  } else {
+    image.setAttribute("src", "./assets/avatar.png")
+    image.setAttribute(
+      "alt",
+      "Foto de Fernando Fantinel sorrindo usando óculos de grau, alargadores, camiseta preta e barba em fundo amarelo."
+    )
+  }
+}
